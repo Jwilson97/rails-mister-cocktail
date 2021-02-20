2bebca2f119425e765bc16926074dee6a3d2826f
jeremy wilson <jeremyw9187@gmail.com>
2021-02-20 15:36:26 -0500

    all routes created for cocktails, finishing up the doses

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -6,4 +6,24 @@ class CocktailsController < ApplicationController
   def show
     @cocktails = Cocktail.find(params[:id])
   end
+
+  def new
+    @cocktails = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to new_cocktail_dose_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
 end
